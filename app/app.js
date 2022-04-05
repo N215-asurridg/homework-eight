7f3e62fa57955fdf001df9c99e3b718253320f5b
CasperMerci <alec.surridge@gmail.com>
2022-04-04 21:50:43 -0400

    the whole project

---
+++ app/app.js
@@ -0,0 +1,240 @@
+var homeContent = `
+<div class="header">
+        <div class="title"><h1>LOVEHARD RECORDS &copy;</h1></div>
+      </div>
+      <div class="paragraph-container">
+        <div class="paragraph">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat a
+            fugiat ad maxime aspernatur modi eum corrupti doloremque, error
+            doloribus eveniet repudiandae esse, nostrum in. Vel eligendi quos
+            ipsa voluptatem.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat a
+            fugiat ad maxime aspernatur modi eum corrupti doloremque, error
+            doloribus eveniet repudiandae esse, nostrum in. Vel eligendi quos
+            ipsa voluptatem.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat a
+            fugiat ad maxime aspernatur modi eum corrupti doloremque, error
+            doloribus eveniet repudiandae esse, nostrum in. Vel eligendi quos
+            ipsa voluptatem.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat a
+            fugiat ad maxime aspernatur modi eum corrupti doloremque, error
+            doloribus eveniet repudiandae esse, nostrum in. Vel eligendi quos
+            ipsa voluptatem.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat a
+            fugiat ad maxime aspernatur modi eum corrupti doloremque, error
+            doloribus eveniet repudiandae esse, nostrum in. Vel eligendi quos
+            ipsa voluptatem.
+          </p>
+        </div>
+      </div>
+      <div class="image-gallery-container">
+        <div class="image-gallery">
+          <div class="tour1"></div>
+          <div class="tour2"></div>
+          <div class="tour3"></div>
+          <div class="tour4"></div>
+        </div>
+      </div>
+`;
+
+var aboutContent = `
+<div class="about-title">
+<h1>ABOUT:</h1>
+<hr />
+</div>
+<div class="about-paragraph">
+<p>
+  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+  ab illo inventore veritatis et quasi architecto beatae vitae dicta
+  sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+  aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+  qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+  dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+  quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+  aliquam quaerat voluptatem.
+</p>
+</div>
+<div class="about-paragraph2">
+<p>
+  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+  ab illo inventore veritatis et quasi architecto beatae vitae dicta
+  sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+  aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+  qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+  dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+  quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+  aliquam quaerat voluptatem.
+</p>
+</div>
+<div class="artist-quote-container">
+<div class="artist-header">
+  <h1>ARTISTS:</h1>
+  <hr />
+</div>
+<div class="artist-photo-quote-container">
+  <div class="artist-image">
+    <img
+      src="images/casper.jpg"
+      alt="casper"
+      style="width: 250px; height: 250px"
+    />
+  </div>
+  <div class="artist-quote">
+    <p>
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+      quae ab illo inventore veritatis et quasi architecto beatae vitae
+      dicta sunt explicabo.
+    </p>
+  </div>
+  <div class="name-tag">- Casper Merci</div>
+</div>
+</div>
+<div class="artist-quote-container">
+<div class="artist-photo-quote-container">
+  <div class="artist-image">
+    <img
+      src="images/artist2.JPG"
+      alt="casper"
+      style="width: 250px; height: 250px"
+    />
+  </div>
+  <div class="artist-quote">
+    <p>
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+      quae ab illo inventore veritatis et quasi architecto beatae vitae
+      dicta sunt explicabo.
+    </p>
+  </div>
+  <div class="name-tag">- Kanye East</div>
+</div>
+</div>
+<div class="artist-quote-container">
+<div class="artist-photo-quote-container">
+  <div class="artist-image">
+    <img
+      src="images/avery.jpg"
+      alt="casper"
+      style="width: 250px; height: 250px"
+    />
+  </div>
+  <div class="artist-quote">
+    <p>
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+      quae ab illo inventore veritatis et quasi architecto beatae vitae
+      dicta sunt explicabo.
+    </p>
+  </div>
+  <div class="name-tag">- Avery McGuire</div>
+</div>
+</div>
+`;
+
+var productsContent = `
+<div class="products-page">
+        <div class="products-title">
+          <div class="title">
+            <h1>PRODUCTS:</h1>
+            <hr />
+          </div>
+        </div>
+        <div class="product-gallery">
+          <div class="img-1"></div>
+          <div class="img-2"></div>
+          <div class="img-3"></div>
+          <div class="img-4"></div>
+        </div>
+        <div class="product-gallery">
+          <div class="img-1"></div>
+          <div class="img-2"></div>
+          <div class="img-3"></div>
+          <div class="img-4"></div>
+        </div>
+      </div>`;
+
+var contactContent = `
+<div class="contact-page">
+        <div class="contact-title-container">
+          <div class="contact-title">
+            <h1>CONTACT US:</h1>
+            <hr />
+          </div>
+        </div>
+        <div class="box-container-container">
+          <div class="box-container">
+            <div class="map"></div>
+            <div class="form-input">
+              <input
+                type="input-box"
+                class="long-box"
+                id="name"
+                placeholder="Your name..."
+              />
+              <input
+                type="input-box"
+                class="long-box"
+                id="email"
+                placeholder="Your email..."
+              />
+              <input
+                type="input-box"
+                class="long-box"
+                id="company"
+                placeholder="Company..."
+              />
+              <input
+                type="input-box"
+                class="tall-box"
+                id="message"
+                placeholder="Message..."
+              />
+            </div>
+          </div>
+        </div>
+        <div class="button-container">
+          <div class="button">
+            <p>SUBMIT</p>
+          </div>
+        </div>
+      </div>
+`      
+
+
+
+function initListeners() {
+    $("nav a").click(function (e) {
+        let btnID = e.currentTarget.id;
+        loadPageContent(btnID);
+        
+        
+        // console.log("click" + contentID);
+    });
+}
+
+function loadPageContent(pageName){
+    let contentID = pageName + "Content";
+    $("#application").html(eval(contentID));
+    
+}
+
+function initApp() {
+    initListeners();
+}
+
+$(document).ready(function () {
+    loadPageContent("home");
+    initApp();
+});
